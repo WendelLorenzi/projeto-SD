a8ba5e82cf5a028c6043153b27c64e5745430b84
WendelLorenzi <wlorenzi17@gmail.com>
2023-11-17 14:16:16 -0300

    feat: implement user for ip

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-
 const path = require('path');
-
 const app = express();
 
-const PORT = 3000;
-
-const HOST = '0.0.0.0';
-
 const server = require('http').createServer(app);
+const PORT = 8080;
 
 const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    },
     allowEIO3: true
 });
 
@@ -22,21 +22,77 @@ app.engine('html', require('ejs').renderFile);
 
 app.set('view engine', 'html');
 
-app.use('/', (req, res) => {
-    res.render('index.html');
+app.get('/', (req, res) => {
+    res.render('index.ejs', { host: process.env.NODE_HOST, port: PORT});
 });
 
+
+
 let messages = [];
+let userIP = [];
+
 
 io.on('connection', socket => {
-    console.log(`socket conectado: ${socket.id}`);
+    const clientIp = socket.handshake.headers['x-real-ip'] || socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
+    console.log('Endereço IP do cliente conectado: ' + clientIp);
+    const getClient = (clientIp) => {
+        return userIP.find(i => i.ip === clientIp && i.author !== undefined);
+    };
+
+    const pushUser = (data) => {
+        if (data.author != undefined) {
+            const objIp = { ip: clientIp, author: data.author };
+            // messages.push({author: data.author, message: data.message})
+            if (objIp.ip && objIp.author) {
+                userIP.push(objIp);
+                // socket.emit('userProfile', objIp);
+                return objIp;
+            }
+        }
+        if (client != undefined) {
+            const obj = { author: client.author, message: data.message };
+            if (client && obj) {
+                messages.push(obj);
+            }
+            return client;
+        }
+    };
+
+    const setMessage = (client, data) => {
+        return new Promise((resolve) => {
+            if (client != undefined && client.author) {
+                // recebendo mensagens
+                    const obj = { author: client.author, message: data.message };
+                    if (client && obj) {
+                        resolve(obj);
+                    }
+            } else {
+                resolve(null);
+            }
+        });
+    }
+
+    let client = getClient(clientIp);
+
+    socket.on('sendMessage', async data => {
+        if (client === undefined) {
+            client = pushUser(data);
+            const result = await setMessage(client, data);
+            if (result !== null) {
+                messages.push(result);
+                // emite as mudanças na variavel messages
+                socket.broadcast.emit('stateChanged', messages);
+            }
+        } else {
+            const result = await setMessage(client, data);
+            if (result !== null) {
+                messages.push(result);
+                socket.broadcast.emit('stateChanged', messages);
+            }
+        }
+    });
 
     socket.emit('previousMessages', messages);
 
-    socket.on('sendMessage', data => {
-        messages.push(data);
-        socket.broadcast.emit('receivedMessage', data);
-    });
 });
-
-server.listen(PORT, HOST);
+server.listen(PORT, `${process.env.NODE_HOST}`);
